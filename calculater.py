2c53db27d0351cb8936fdf02c798f44ef4aab7d2
Sibtul Hassan <hassan0007267@gmail.com>
2024-08-07 15:10:21 +0500

    first commit

---
+++ calculater.py
@@ -0,0 +1,15 @@
+num1= int (input("enter your first number: "))
+num2=int(input("enter your second number"))
+opr=input("enter the opr..(+,-,*,/)")
+if opr=='+':
+    print(num1+num2)
+elif opr=="-":
+    print(num1-num2)
+elif opr=="/":
+    print(num1/num2)
+elif opr=="*":
+    print(num1*num2)
+else:
+    print("invali oprations")     
+     
+
